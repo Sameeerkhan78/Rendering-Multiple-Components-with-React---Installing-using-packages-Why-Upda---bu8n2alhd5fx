04385fd00f1264480fb47cca12d313d06462c461
Sameer Khan <115018276+Sameeerkhan78@users.noreply.github.com>
2022-12-10 15:58:41 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,25 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
 
-const App = () => {
-  return (
-    <div id="main"></div>
-  )
+class App extends Component {
+    constructor(props) {
+		super(props);
+	    this.state = {
+	    text: "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy",
+		    click : 0
+	    
+	    }
+	};
+
+    render() {
+    	return(
+    		<div id="main">
+	{ /* Do not remove this main div!! */ }
+	<button id="click" onClick={() => this.setState({count: this.state.count + 1 })}>Button</button>
+    <h6 id="para">{this.state.text}</h6>
+    		</div>
+    	);
+    }
 }
 
 
